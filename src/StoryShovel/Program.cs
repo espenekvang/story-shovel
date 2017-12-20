892da7a173bfe5fd634ee685236bbb94c9fd8eb1
Espen Ekvang <espen.ekvang@bekk.no>
2017-12-20 16:32:19 +0100

    Initial commit

---
+++ src/StoryShovel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using StoryShovel.VsTs;
+
+namespace StoryShovel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static Task MainAsync()
+        {
+            var appsettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var vstsBoard = new VsTsBoard(appsettings["VSTS:PrivateAccessToken"], appsettings["VSTS:TeamBaseAddress"], appsettings["VSTS:ProjectName"]);
+            return vstsBoard.AddUserStory("A userstory from console app", "The description of the story from console app");
+        }
+    }
+}
---
+++ src/StoryShovel/VsTs/VsTsWorkObject.cs
@@ -0,0 +1,15 @@
+﻿namespace StoryShovel.VsTs
+{
+    internal class VsTsWorkObject
+    {
+        public string Op { get; }
+        public string Path { get; }
+        public string Value { get; }
+        public VsTsWorkObject(string op, string path, string value)
+        {
+            Op = op;
+            Path = path;
+            Value = value;
+        }
+    }
+}
